5952252a4db27298714caca527262fd030551fef
ggmaddr <ggiangta@gmail.com>
2023-02-15 05:14:20 -0800

    ejs

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+app.get('/index', (req, res)=>{
+    // res.render('index');
+    res.send('succeed')
+})
+app.listen(3000, () => {
+    console.log("APP IS LISTENING ON PORT 3000!")
+})
